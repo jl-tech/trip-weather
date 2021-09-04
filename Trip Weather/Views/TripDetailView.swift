2a7bf0ffedc94a05a2fdc106c7862837c17b2aff
Jonathan Liu <jonathan.liu1@student.unsw.edu.au>
2021-09-05 00:25:42 +1000

    Add basic design for trip detail view

--- Trip Weather/Views/TripDetailView.swift
+++ Trip Weather/Views/TripDetailView.swift
@@ -15,33 +15,60 @@ struct TripDetailView: View {
         ScrollView {
             LazyVStack {
                 ForEach (trip.locations) { location in
+                    if locationIsOnNewDate(location) {
+                        Text(toLongDateString(from: location.day).uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(.gray)
+                    }
                     WeatherCard(location: location)
                 }
             }.onAppear {
                 viewModel.loadWeatherForTrip(trip)
             }
-        }
+        }.navigationTitle(trip.name)
     }
     
-    
-    
+    private func locationIsOnNewDate(_ location: TripsViewModel.Location) -> Bool {
+        if let idx = trip.locations.firstIndex(where: { $0.id == location.id }) {
+            if idx == 0 {
+                return true
+            }
+            else {
+                if trip.locations[idx - 1].day == location.day {
+                    return false
+                } else {
+                    return true
+                }
+            }
+        }
+        return false
+    }
 }
 
 struct WeatherCard: View {
     let location: TripsViewModel.Location
     
     var body: some View {
-        ZStack {
+        ZStack(alignment: .top) {
             RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-                .foregroundColor(.white)
+                .foregroundColor(.blue)
             if location.weatherLoadStatus == .loading {
                 ProgressView()
             }
             else {
-                Text(String(describing: location.forecast))
+                VStack(alignment: .leading) {
+                    Text(location.name)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.all)
+                    Text(String(describing: location.forecast))
+                }
             }
-                
         }
+        .frame(height: 300)
+        .padding([.bottom, .leading, .trailing])
         
     }
 }
